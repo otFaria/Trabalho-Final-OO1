d3d1d472ede33df97fba2a8a7a91eff7ea796b94
joaocarlosms <jcmeseverino@gmail.com>
2024-08-29 16:26:59 -0300

    add postgre connector

---
+++ src/main/java/Connection/database/connection/PostgreSQLConnector.java
@@ -0,0 +1,54 @@
+package Connection.database.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class PostgreSQLConnector {
+
+    private Connection connection;
+
+    public PostgreSQLConnector(String dbName) throws SQLException {
+        String urlConnection = "jdbc:postgresql://localhost:8080/" + dbName;
+        this.connection = DriverManager.getConnection(urlConnection);
+
+        createTableBook();
+        createTableAuthor();
+    }
+
+    public void createTableBook() {
+        String sql = "CREATE TABLE IF NOT EXISTS book("
+                + "id SERIAL PRIMARY KEY,"
+                + "name VARCHAR(200) NOT NULL,"
+                + "author_id INT REFERENCES author(id) ON DELETE CASCADE" +
+                ");";
+
+        try(Statement stmt = connection.createStatement()) {
+            stmt.executeUpdate(sql);
+
+        } catch (SQLException e) {
+            System.out.println("Error connection database"+ e.getMessage());
+        }
+    }
+
+    public void createTableAuthor() {
+        String sql = "CREATE TABLE IF NOT EXISTS author("
+                + "id SERIAL PRIMARY KEY,"
+                + "name VARCHAR(200) NOT NULL,"
+                + "cpf VARCHAR(15) NOT NULL,"
+                + "hometown VARCHAR(200) NOT NULL" +
+                ");";
+
+        try(Statement stmt = connection.createStatement()) {
+            stmt.executeUpdate(sql);
+
+        } catch(SQLException e) {
+            System.out.println("Error connection database"+ e.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
