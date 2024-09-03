bc040ecaf81947ea37a48e9ebeeac4dad9327db3
joaocarlosms <jcmeseverino@gmail.com>
2024-09-03 19:46:25 -0300

    feat: add all methods on book table model

--- src/main/java/View/JTableModel/TMBook.java
+++ src/main/java/View/JTableModel/TMBook.java
@@ -4,6 +4,8 @@
  */
 package View.JTableModel;
 
+import Model.entitites.Book;
+import java.util.List;
 import javax.swing.table.AbstractTableModel;
 
 /**
@@ -11,20 +13,74 @@ import javax.swing.table.AbstractTableModel;
  * @author jc
  */
 public class TMBook extends AbstractTableModel {
-
+    private List listBooks;
+    
+    private final int COL_COD_BOOK = 0;
+    private final int COL_NAME = 1;
+    private final int COL_AUTHOR_ID = 2;
+    
     @Override
     public int getRowCount() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return listBooks.size();
     }
 
     @Override
     public int getColumnCount() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return 3;
     }
 
     @Override
-    public Object getValueAt(int i, int i1) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Book aux = new Book();
+        
+        //return object empty
+        if(listBooks.isEmpty()) {
+            return aux;
+        } else {
+            aux = (Book) listBooks.get(rowIndex);
+            
+            switch(columnIndex) {
+                case -1:
+                    return aux;
+                case COL_COD_BOOK:
+                    return aux.getCod_book();
+                case COL_NAME:
+                    return aux.getName();
+                case COL_AUTHOR_ID:
+                    return aux.getAuthorId();
+                default:
+                    break;
+            }
+        }
+        
+        return aux;
     }
     
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return false;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        
+        switch (column) {
+            case COL_COD_BOOK:
+                return "Cod_Book";
+            case COL_NAME:
+                return "Name";
+            case COL_AUTHOR_ID:
+                return "Author_id"; 
+            default:
+                break;
+        }
+
+        return "";
+    }
+
+    @Override
+    public Class getColumnClass(int columnIndex) {        
+        return String.class;
+    }
+
 }
