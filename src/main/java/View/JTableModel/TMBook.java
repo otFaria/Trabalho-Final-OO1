966ec7334f04d6397c41a63450bb104669b097e7
joaocarlosms <jcmeseverino@gmail.com>
2024-09-03 18:39:11 -0300

    feat: add abstract table model

---
+++ src/main/java/View/JTableModel/TMBook.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View.JTableModel;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author jc
+ */
+public class TMBook extends AbstractTableModel {
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object getValueAt(int i, int i1) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/main/java/View/JTableModel/TMLibrary.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View.JTableModel;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author jc
+ */
+public class TMLibrary extends AbstractTableModel {
+    
+}
