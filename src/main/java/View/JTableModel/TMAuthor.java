6a5900aea7dc9c17d6e7ed1ee25040d8b41d9798
joaocarlosms <jcmeseverino@gmail.com>
2024-09-03 19:22:25 -0300

    feat: add all methods in author table model

--- src/main/java/View/JTableModel/TMAuthor.java
+++ src/main/java/View/JTableModel/TMAuthor.java
@@ -4,10 +4,90 @@
  */
 package View.JTableModel;
 
+import Model.entitites.Author;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
 /**
  *
  * @author jc
  */
 public class TMAuthor extends AbstractTableModel {
     
+    private List listAuthors;
+    
+    private final int COL_NAME = 0;
+    private final int COL_CPF = 1;
+    private final int COL_HOMETOWN = 2;
+
+    public TMAuthor(List listAuthors) {
+        this.listAuthors = listAuthors;
+    }
+    
+    @Override
+    public int getRowCount() {
+        return listAuthors.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+    
+    // receive the value selected in your paramater
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Author aux = new Author();
+        
+        if(listAuthors.isEmpty()) {
+            return aux;
+        } else {
+            aux = (Author) listAuthors.get(rowIndex);
+            
+            switch(columnIndex) {
+                case -1:
+                    return aux;
+                case COL_NAME:
+                    return aux.getName();
+                case COL_CPF:
+                    return aux.getCpf();
+                case COL_HOMETOWN:
+                    return aux.getHometown();
+                default:
+                    break;
+            }
+        }
+        return aux;
+    }
+
+    //Check cell of table is editable or not editable
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return false;
+    }
+    
+    
+    //return name of column
+    @Override
+    public String getColumnName(int column) {
+        
+        switch(column) {
+            case COL_NAME:
+                return "name";
+            case COL_CPF:
+                return "cpf";
+            case COL_HOMETOWN:
+                return "hometown";
+            default:
+                break;
+        }
+        
+        return "";
+    }
+    
+    //return class of column
+    @Override
+    public Class getColumnClass(int columnIndex) {
+        return String.class;
+    }
 }
--- src/main/java/View/JTableModel/TMLibrary.java
+++ src/main/java/View/JTableModel/TMLibrary.java
@@ -11,5 +11,20 @@ import javax.swing.table.AbstractTableModel;
  * @author jc
  */
 public class TMLibrary extends AbstractTableModel {
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object getValueAt(int i, int i1) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
